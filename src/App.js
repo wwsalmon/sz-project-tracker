b2bd19c8595d27a3b86569172368e7c07b2f8992
wwsalmon <wwsamson@yahoo.com>
2020-05-25 15:47:09 -0400

    add more ui elements

--- src/App.js
+++ src/App.js
@@ -124,8 +124,8 @@ export default class App extends Component {
   render() {
     const { authState } = this.state;
     return (
-      <div className="App m-4">
-        {authState === "loading" && (<p>Loading...</p>)}
+      <div className="max-w-6xl mx-auto px-2">
+        {authState === "loading" && (<p className="aside ~info my-4">Loading...</p>)}
         {authState === "signIn" && (
           <>
             <button className="button !normal ~neutral my-4" onClick={() => {
@@ -148,12 +148,20 @@ export default class App extends Component {
             {this.state.projState === "loading" && (
               <p>Loading projects...</p>
             )}
-            {this.state.projState === "ready" &&
-              this.state.projects.map((project, index) => (
-                <div key={index} className="card my-4">
-                  <p>{project.name}</p>
+            {this.state.projState === "ready" && (
+              <>
+                <button className="button !normal ~neutral my-4" onClick={() => this.signOut()}>New Project</button>
+                <div class="project-container grid md:grid-cols-2 gap-2 lg:grid-cols-3">
+                  {this.state.projects.map((project) => (
+                    <div key={project.id} className="card border">
+                      <p>{project.name}</p>
+                      <button className="button">Delete</button>
+                    </div>
+                  ))}
                 </div>
-              ))
+              </>
+
+            )
             }
           </>
         )}
