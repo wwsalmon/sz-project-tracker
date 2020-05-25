7edf9665cf9191f046c1db707f62dd9e4564a61a
wwsalmon <wwsamson@yahoo.com>
2020-05-25 15:30:13 -0400

    add loading states, sign out process

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ const query = `
 `
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       projects: [],
-      user: false,
+      authState: "loading",
+      projState: "none",
       username: "sztest",
       password: "Passw0rd!",
       code: ""
@@ -28,109 +29,135 @@ export default class App extends Component {
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
   }
 
-  handleFieldUsername(e){
-    this.setState({username: e.target.value});
+  componentDidMount() {
+    Auth.currentAuthenticatedUser().then(() => {
+      this.setState({ authState: "signedIn" });
+      this.loadProjects();
+    }).catch(e => {
+      console.log(e);
+      this.setState({ authState: "signIn" })
+    })
   }
 
-  handleFieldPassword(e){
-    this.setState({password: e.target.value});
+  handleFieldUsername(e) {
+    this.setState({ username: e.target.value });
   }
 
-  async handleLoginSubmit(e){
+  handleFieldPassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  async handleLoginSubmit(e) {
     e.preventDefault();
-    console.log(this.state.username, this.state.password);
-    this.setState({user: await this.signIn()})
+    this.signIn();
   }
 
-  async signUp() {
-    const { username, password, email } = this.state;
-    try {
-      const user = await Auth.signUp({
-        username, password,
-        attributes: {
-          email,
-        }
-      });
-      console.log({ user })
-    } catch (error) {
-      console.log('error signing up: ', error);
-    }
-  }
+  // async signUp() {
+  //   const { username, password, email } = this.state;
+  //   try {
+  //     const user = await Auth.signUp({
+  //       username, password,
+  //       attributes: {
+  //         email,
+  //       }
+  //     });
+  //     console.log({ user })
+  //   } catch (error) {
+  //     console.log('error signing up: ', error);
+  //   }
+  // }
 
-  async confirmSignUp() {
-    const { username, code } = this.state;
-    try {
-      await Auth.confirmSignUp(username, code);
-    } catch (error) {
-      console.log('error confirming sign up', error);
-    }
-  }
+  // async confirmSignUp() {
+  //   const { username, code } = this.state;
+  //   try {
+  //     await Auth.confirmSignUp(username, code);
+  //   } catch (error) {
+  //     console.log('error confirming sign up', error);
+  //   }
+  // }
 
   async signIn() {
-    console.log(this.state);
+    this.setState({ authState: "loading" });
     const { username, password } = this.state;
     try {
-      const user = await Auth.signIn(username, password);
-      return user;
+      await Auth.signIn(username, password).then(() => {
+        this.setState({ authState: "signedIn" });
+        this.loadProjects();
+      });
     } catch (error) {
       console.log('error signing in', error);
-      return false;
+      this.setState({ authState: "signIn" });
     }
   }
 
-  async resendConfirmationCode() {
-    const { username } = this.state;
-    try {
-      await Auth.resendSignUp(username);
-      console.log('code resent succesfully');
-    } catch (err) {
-      console.log('error resending code: ', err);
-    }
-  }
+  // async resendConfirmationCode() {
+  //   const { username } = this.state;
+  //   try {
+  //     await Auth.resendSignUp(username);
+  //     console.log('code resent succesfully');
+  //   } catch (err) {
+  //     console.log('error resending code: ', err);
+  //   }
+  // }
 
   async signOut() {
     try {
       await Auth.signOut();
-    } catch (error) {
+      this.setState({ authState: "signIn" });
+    }
+    catch (error) {
       console.log('error signing out: ', error);
     }
   }
 
   async loadProjects() {
+    this.setState({
+      projState: "loading"
+    })
     const data = await API.graphql(graphqlOperation(query));
     this.setState({
-      projects: data.data.listProjects.items
+      projects: data.data.listProjects.items,
+      projState: "ready"
     })
   }
 
   render() {
-    if (this.state.user) {
-      this.loadProjects();
-      return (
-        <>
-          <h3>Logged in!</h3>
-          {
-            this.state.projects.map((project, index) => (
-              <p key={index}>{project.name}</p>
-            ))
-          }
-        </>
-      )
-    }
-    else {
-      return (
-        <>
-          <button className="button !normal ~neutral m-4">Sign in with Google</button>
-          <h3>Or, login with username</h3>
-          <form onSubmit={this.handleLoginSubmit}>
-            <p className="label">Username:</p>
-            <input type="text" className="field ~neutral !normal w-auto my-1" value={this.state.username} onChange={this.handleFieldUsername}></input>
-            <p className="label">Password:</p>
-            <input type="password" className="field ~neutral !normal w-auto my-1" value={this.state.password} onChange={this.handleFieldPassword}></input>
-            <input type="submit" className="button field w-auto block my-4"></input>
-          </form>
-        </>
-      )
-    }
+    const { authState } = this.state;
+    return (
+      <div className="App m-4">
+        {authState === "loading" && (<p>Loading...</p>)}
+        {authState === "signIn" && (
+          <>
+            <button className="button !normal ~neutral my-4" onClick={() => {
+              Auth.federatedSignIn({ provider: 'Google' });
+            }}>Sign in with Google</button>
+            <h3>Or, login with username</h3>
+            <form onSubmit={this.handleLoginSubmit}>
+              <p className="label">Username:</p>
+              <input type="text" className="field ~neutral !normal w-auto my-1" value={this.state.username} onChange={this.handleFieldUsername}></input>
+              <p className="label">Password:</p>
+              <input type="password" className="field ~neutral !normal w-auto my-1" value={this.state.password} onChange={this.handleFieldPassword}></input>
+              <input type="submit" className="button field w-auto block my-4"></input>
+            </form>
+          </>
+        )}
+        {authState === "signedIn" && (
+          <>
+            <p className="aside ~info my-4">Logged in!</p>
+            <button className="button !normal ~neutral my-4" onClick={() => this.signOut()}>Log out</button>
+            {this.state.projState === "loading" && (
+              <p>Loading projects...</p>
+            )}
+            {this.state.projState === "ready" &&
+              this.state.projects.map((project, index) => (
+                <div key={index} className="card my-4">
+                  <p>{project.name}</p>
+                </div>
+              ))
+            }
+          </>
+        )}
+      </div>
+    )
   };
 }
