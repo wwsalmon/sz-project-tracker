053d02f4f31aba00b017e4a5d302fdbc13c93c18
Rishav20-22 <52622186+Rishav20-22@users.noreply.github.com>
2020-06-21 12:36:07 +0530

    add modal using tailwind.css
    Adding modal using tailwind.css

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { API, graphqlOperation, Auth } from 'aws-amplify';
 
+
+
 export default function Projects(props) {
     const history = useHistory();
+    const [showModal, setShowModal] = React.useState(false);
+
     // const [isLoading, setIsLoading] = useState(true);
     const [isInit, setIsInit] = useState(false);
     const [projects, setProjects] = useState([]);
@@ -30,7 +34,10 @@ export default function Projects(props) {
             // setIsLoading(false);
             console.log(error);
         }
+        window.location.reload();
+
     }
+    
 
     useEffect(() => {
         console.log("running projects useeffect");
@@ -47,6 +54,7 @@ export default function Projects(props) {
         function loadProjects() {
             return API.graphql(graphqlOperation(listQuery));
         }
+       
 
         async function onLoad() {
             try {
@@ -66,8 +74,10 @@ export default function Projects(props) {
 
         onLoad();
     }, [history.push]);
+   
 
     return (
+        
         <div>
             {/* {isLoading && (
                 <p className="aside ~info">Loading...</p>
@@ -86,7 +96,60 @@ export default function Projects(props) {
                         {projects.data.listProjects.items.map((project) => (
                             <div key={project.id} className="card border">
                                 <Link to={`/projects/${project.id}`}><p>{project.name}</p></Link>
-                                <button className="button" onClick={(e) => deleteProject(e, project.id)}>Delete</button>
+                                
+                                
+                            
+      <button
+        className="bg-red-600 text-white active:bg-pink-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+        type="button"
+        style={{ transition: "all .15s ease" }}
+        onClick={() => setShowModal(true)}
+      >
+        Delete Project
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Deleting a Project
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-gray-600 text-lg leading-relaxed">
+                    Do you want to delete this project?
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+                 
+                  <br></br>
+             <button className="bg-red-600 text-white active:bg-pink-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" onClick={(e) => deleteProject(e, project.id)}>Delete</button> 
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    
                             </div>
                         ))}
                     </div>
@@ -96,4 +159,5 @@ export default function Projects(props) {
             )}
         </div>
     )
+    
 }
