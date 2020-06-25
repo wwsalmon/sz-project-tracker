eb4d34f7c443a014339661a48f2d2902b82d2c55
Samson Zhang <wwsamson@yahoo.com>
2020-06-25 19:57:53 -0400

    add footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Routes from "./Routes";
 import Navbar from "./components/Navbar";
 import {ProvideAuth} from "./lib/authLib";
 import "./App.css";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
   <ProvideAuth>
-      <Navbar></Navbar>
+      <Navbar/>
       <div className="max-w-6xl mx-auto px-4 pt-24">
         <Routes />
       </div>
+      <Footer/>
   </ProvideAuth>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div className="w-full border-t mt-16">
+            <div className="max-w-6xl mx-auto h-16 flex items-center px-4 opacity-50 content">
+                <p>Project tracker by <a href="https://twitter.com/wwsalmon">Samson Zhang</a> at <a
+                    href="https://summerofshipping.com/">Summer of Shipping 2020</a>.</p>
+            </div>
+        </div>
+    )
+}
