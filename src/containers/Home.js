96365a163c95bb1a9c48c87e1d8972f502f45d9e
Samson Zhang <wwsamson@yahoo.com>
2020-06-25 19:39:56 -0400

    make homepage prettier

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,8 +4,14 @@ import {Link} from 'react-router-dom';
 export default function Home(){
     return(
         <>
-            <Link to="/login"><button className="button !low ~positive my-4">Log in</button></Link>
-            <Link to="/signup"><button className="button !high ~positive my-4">Sign up</button></Link>
+            <div className="text-center">
+                <h1 className="heading my-8">Project Tracker</h1>
+                <p className="max-w-3xl mx-auto content">Made by <a href="https://twitter.com/wwsalmon">Samson Zhang</a> at <a
+                    href="https://summerofshipping.com/">Summer of Shipping 2020</a>.</p>
+                <p className="max-w-3xl mx-auto my-8 aside ~warning !normal ">Development version, don't actually use this to store data you care about!</p>
+                <Link to="/login"><button className="button !low ~info my-4">Log in</button></Link>
+                <Link to="/signup"><button className="button !high ~info my-4">Sign up</button></Link>
+            </div>
         </>
     )
 }
