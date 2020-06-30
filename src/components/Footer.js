1765b865092c6db3855804a44e315ed02b9a1e1d
Samson Zhang <wwsamson@yahoo.com>
2020-06-30 15:27:09 -0400

    add rishav to footer credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer(){
     return (
         <div className="w-full border-t mt-16">
             <div className="max-w-6xl mx-auto h-16 flex items-center px-4 opacity-50 content">
-                <p>Project tracker by <a href="https://twitter.com/wwsalmon">Samson Zhang</a> at <a
+                <p>Project tracker by <a href="https://twitter.com/wwsalmon">Samson Zhang</a> and <a
+                    href="https://twitter.com/RishavK45">Rishav Kumar</a> at <a
                     href="https://summerofshipping.com/">Summer of Shipping 2020</a>.</p>
             </div>
         </div>
