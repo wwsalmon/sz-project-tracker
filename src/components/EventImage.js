1803678333e911fbf47eb7c1687660e9ae8c8b1a
Samson Zhang <wwsamson@yahoo.com>
2020-07-06 16:59:13 -0400

    refactor eventimage with lightbox

--- src/components/EventImage.js
+++ src/components/EventImage.js
@@ -1,8 +1,12 @@
 import { Storage } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
 export default function EventImage(props){
     const [imgUrl, setImgUrl] = useState("");
+    const [showLightbox, setShowLightbox] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
@@ -27,7 +31,24 @@ export default function EventImage(props){
                 <p>Loading...</p>
             )}
             {!isLoading && (
-                <img alt="Update attachment" src={imgUrl}></img>
+                <div className={showLightbox ? "z-10 fixed inset-0 flex justify-center items-center" : ""}>
+                    <img onClick={() => !showLightbox && setShowLightbox(true)}
+                         alt="Update attachment"
+                         src={imgUrl}
+                         className={showLightbox ? "z-20 max-h-full max-w-full p-4" : "cursor-pointer"}/>
+                    {showLightbox && (
+                        <>
+                            <div className="fixed top-4 z-30 cursor-pointer right-4 shield ~neutral !normal"
+                                 onClick={() => setShowLightbox(false)}>
+                                <span className="icon">
+                                    <FontAwesomeIcon icon={faTimes}/>
+                                </span>
+                            </div>
+                            <div onClick={() => setShowLightbox(false)}
+                                 className="opacity-50 fixed inset-0 z-10 bg-black cursor-pointer"/>
+                        </>
+                    )}
+                </div>
             )}
         </div>
     )
