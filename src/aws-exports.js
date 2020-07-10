6da37052d6272b775eaf4fd76056f88c18632fb9
Samson Zhang <wwsamson@yahoo.com>
2020-07-10 08:41:18 -0400

    change google signin redirect to live site

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/googleredirect",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://sz-project-tracker-v0.netlify.app/googleredirect",
+        "redirectSignOut": "https://sz-project-tracker-v0.netlify.app/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
